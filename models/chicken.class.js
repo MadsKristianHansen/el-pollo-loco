7e88aaee1303724f1d9378a2f2d11dedede92598
Mads Kristian Hansen <mads_hansen@hotmail.de>
2022-08-26 13:17:39 +0200

    feat: randomizes spawnpoints of enemies

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -3,5 +3,7 @@ class Chicken extends MovableObject {
     super().loadImage(
       "img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/2-Ga_centro.png"
     );
+
+    this.x = 200 + Math.random() * 500;
   }
 }
