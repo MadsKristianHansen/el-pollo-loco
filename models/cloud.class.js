ecef500f4402eb98b4f8001d0c513d829b838e54
Mads Kristian Hansen <mads_hansen@hotmail.de>
2022-08-27 10:30:23 +0200

    feat: animates clouds

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,5 +7,12 @@ class Cloud extends MovableObject {
     super().loadImage("img/5.Fondo/Capas/4.nubes/1.png");
 
     this.x = Math.random() * 500;
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.x -= 0.15;
+    }, 1000 / 60);
   }
 }
