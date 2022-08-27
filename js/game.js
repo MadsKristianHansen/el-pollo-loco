d83503b617aaa134a2dd974463d4183b9ef23e18
Mads Kristian Hansen <mads_hansen@hotmail.de>
2022-08-27 19:48:15 +0200

    feat: adds keyboardObject and reactive Character movement

--- js/game.js
+++ js/game.js
@@ -1,9 +1,54 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init() {
   canvas = document.getElementById("canvas");
-  world = new World(canvas);
+  world = new World(canvas, keyboard);
 
   console.log("My Character is", world.character);
 }
+
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode == 39) {
+    keyboard.RIGHT = true;
+  }
+
+  if (e.keyCode == 37) {
+    keyboard.RIGHT = true;
+  }
+
+  if (e.keyCode == 38) {
+    keyboard.RIGHT = true;
+  }
+
+  if (e.keyCode == 40) {
+    keyboard.RIGHT = true;
+  }
+
+  if (e.keyCode == 32) {
+    keyboard.RIGHT = true;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  if (e.keyCode == 39) {
+    keyboard.RIGHT = false;
+  }
+
+  if (e.keyCode == 37) {
+    keyboard.RIGHT = false;
+  }
+
+  if (e.keyCode == 38) {
+    keyboard.RIGHT = false;
+  }
+
+  if (e.keyCode == 40) {
+    keyboard.RIGHT = false;
+  }
+
+  if (e.keyCode == 32) {
+    keyboard.RIGHT = false;
+  }
+});
--- models/character.class.js
+++ models/character.class.js
@@ -10,6 +10,7 @@ class Character extends MovableObject {
     "../img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-25.png",
     "../img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-26.png",
   ];
+  world;
 
   constructor() {
     super().loadImage(
@@ -22,10 +23,12 @@ class Character extends MovableObject {
 
   animate() {
     setInterval(() => {
-      let i = this.currentImage % this.IMAGES_WALKING.length;
-      let path = this.IMAGES_WALKING[i];
-      this.img = this.imageCache[path];
-      this.currentImage++;
+      if (this.world.keyboard.RIGHT) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+      }
     }, 100);
   }
 
