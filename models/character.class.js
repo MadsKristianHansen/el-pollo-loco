fd1012df612180e7a065b547e175612cb7d79af0
Mads Kristian Hansen <mads_hansen@hotmail.de>
2022-08-30 14:44:02 +0200

    feat: adds jumping animation

--- models/character.class.js
+++ models/character.class.js
@@ -1,7 +1,7 @@
 class Character extends MovableObject {
   height = 280;
   width = 150;
-  y = 80;
+  y = 50;
   speed = 10;
   IMAGES_WALKING = [
     "../img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png",
@@ -11,6 +11,18 @@ class Character extends MovableObject {
     "../img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-25.png",
     "../img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-26.png",
   ];
+  IMAGES_JUMPING = [
+    "img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-31.png",
+    "img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-32.png",
+    "img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-33.png",
+    "img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-34.png",
+    "img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-35.png",
+    "img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-36.png",
+    "img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-37.png",
+    "img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-38.png",
+    "img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-39.png",
+    "img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-40.png",
+  ];
   world;
   walking_sound = new Audio("audio/running.mp3");
 
@@ -19,6 +31,7 @@ class Character extends MovableObject {
       "../img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png"
     );
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_JUMPING);
 
     this.animate();
     this.applyGravity();
@@ -42,8 +55,12 @@ class Character extends MovableObject {
     }, 1000 / 60);
 
     setInterval(() => {
-      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-        this.playAnimation(this.IMAGES_WALKING);
+      if (this.isAboveGround()) {
+        this.playAnimation(this.IMAGES_JUMPING);
+      } else {
+        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+          this.playAnimation(this.IMAGES_WALKING);
+        }
       }
     }, 50);
   }
