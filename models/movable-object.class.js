190d31d51e4fecbdbaf1273b976354ea6a513bb4
Mads Kristian Hansen <mads_hansen@hotmail.de>
2022-08-30 20:13:49 +0200

    refactor & feat: outsources draw and flipImage(Back) methods + adds movable object frames for collision detection

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -51,4 +51,16 @@ class MovableObject {
   moveRight() {
     this.x += this.speed;
   }
+
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  drawFrame(ctx) {
+    ctx.beginPath();
+    ctx.lineWidth = "5";
+    ctx.strokeStyle = "blue";
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.stroke();
+  }
 }
