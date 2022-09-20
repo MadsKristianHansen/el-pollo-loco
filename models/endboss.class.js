2f939d147c9b9b6e2fc195565cfb1ba4e2c32cdf
Mads Kristian Hansen <mads_hansen@hotmail.de>
2022-09-20 19:40:06 +0200

    feat: adds further endboss animations

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -4,6 +4,7 @@ class Endboss extends MovableObject {
   y = 55;
   hurt_chicken_sound = new Audio("audio/chicken_hurt.mp3");
   chickenAlive = true;
+  speed = 50;
 
   IMAGES_ALERT = [
     "img/4.Secuencias_Enemy_gigantón-Doña_Gallinota-/2.Ateción-ataque/1.Alerta/G5.png",
@@ -28,13 +29,34 @@ class Endboss extends MovableObject {
     "img/4.Secuencias_Enemy_gigantón-Doña_Gallinota-/3.Herida/G23.png",
   ];
 
+  IMAGES_WALKING = [
+    "img/4.Secuencias_Enemy_gigantón-Doña_Gallinota-/1.Caminata/G1.png",
+    "img/4.Secuencias_Enemy_gigantón-Doña_Gallinota-/1.Caminata/G2.png",
+    "img/4.Secuencias_Enemy_gigantón-Doña_Gallinota-/1.Caminata/G3.png",
+    "img/4.Secuencias_Enemy_gigantón-Doña_Gallinota-/1.Caminata/G4.png",
+  ];
+
+  IMAGES_RAGE = [
+    "img/4.Secuencias_Enemy_gigantón-Doña_Gallinota-/2.Ateción-ataque/2.Ataque/G13.png",
+    "img/4.Secuencias_Enemy_gigantón-Doña_Gallinota-/2.Ateción-ataque/2.Ataque/G14.png",
+    "img/4.Secuencias_Enemy_gigantón-Doña_Gallinota-/2.Ateción-ataque/2.Ataque/G15.png",
+    "img/4.Secuencias_Enemy_gigantón-Doña_Gallinota-/2.Ateción-ataque/2.Ataque/G16.png",
+    "img/4.Secuencias_Enemy_gigantón-Doña_Gallinota-/2.Ateción-ataque/2.Ataque/G17.png",
+    "img/4.Secuencias_Enemy_gigantón-Doña_Gallinota-/2.Ateción-ataque/2.Ataque/G18.png",
+    "img/4.Secuencias_Enemy_gigantón-Doña_Gallinota-/2.Ateción-ataque/2.Ataque/G19.png",
+    "img/4.Secuencias_Enemy_gigantón-Doña_Gallinota-/2.Ateción-ataque/2.Ataque/G20.png",
+  ];
+
   constructor() {
     super().loadImage(this.IMAGES_ALERT[0]);
     this.loadImages(this.IMAGES_ALERT);
     this.loadImages(this.IMAGES_HIT);
     this.loadImages(this.IMAGES_DEAD);
+    this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_RAGE);
     this.x = 3400;
     this.animate();
+    this.checkPosition();
   }
 
   animate() {
@@ -44,8 +66,32 @@ class Endboss extends MovableObject {
       } else if (this.isHurt()) {
         this.playAnimation(this.IMAGES_HIT);
         this.hurt_chicken_sound.play();
+      } else if (this.energy < 100 && this.energy > 80) {
+        this.playAnimation(this.IMAGES_WALKING);
+        if (this.otherDirection && this.x < 3400) {
+          this.moveRight();
+        } else if (!this.otherDirection && this.x > 150) {
+          this.moveLeft();
+        }
+      } else if (this.energy <= 80) {
+        this.playAnimation(this.IMAGES_RAGE);
+        if (this.otherDirection && this.x < 3400) {
+          this.moveRight();
+        } else if (!this.otherDirection && this.x > 150) {
+          this.moveLeft();
+        }
       } else {
         this.playAnimation(this.IMAGES_ALERT);
+      }
+    }, 200);
+  }
+
+  checkPosition() {
+    setInterval(() => {
+      if (this.x >= 3400) {
+        this.otherDirection = false;
+      } else if (this.x <= 150) {
+        this.otherDirection = true;
       }
     }, 200);
   }
